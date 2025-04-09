e886bbd734dd0e09bc0be16e0050647e1e15ebe6
GuljahanKerimova <st135556@student.spbu.ru>
2025-04-09 12:00:56 +0300

    Update test_transformers.cpp

--- HomeAssignment3/test_transformers.cpp
+++ HomeAssignment3/test_transformers.cpp
@@ -1,44 +1,76 @@
-
-
 #include <gtest/gtest.h>
 #include "Transformers.h"
 #include "autobot.h"
 #include "minicon.h"   
 #include "decepticon.h"  
-#include <iostream>
 
-// Base class test
-TEST(TransformerTest, transformers) {
-    BaseTransformer myRobo2;
-    myRobo2.setName("Titanus");
-    myRobo2.setHeight(150);
-    EXPECT_EQ(myRobo2.getHeight(), 150);
-    EXPECT_EQ(myRobo2.getName(), "Titanus");
+// --- BaseTransformer Tests ---
+TEST(TransformerTest, DefaultValues) {
+    BaseTransformer transformer;
+    EXPECT_EQ(transformer.getName(), "");  
+    EXPECT_EQ(transformer.getHeight(), 0); 
 }
 
-// Autobots
-// We initialized the value of the autobots counter 
-int Autobot::autobotCounter = 0;
-TEST(AutobotTest, autobotsCount) {
+TEST(TransformerTest, SetNameAndHeight) {
+    BaseTransformer transformer;
+    transformer.setName("Optimus");
+    transformer.setHeight(30);
+    EXPECT_EQ(transformer.getName(), "Optimus");
+    EXPECT_EQ(transformer.getHeight(), 30);
+}
+
+// --- Autobot Tests ---
+int Autobot::autobotCounter = 0; // Initialize static counter
+
+TEST(AutobotTest, CounterIncrementsCorrectly) {
+    int initialCount = Autobot::autobotCounter;
+    Autobot bot1, bot2;
+    EXPECT_EQ(Autobot::autobotCounter, initialCount + 2); 
+}
+
+TEST(AutobotTest, InheritsBaseProperties) {
     Autobot autobot;
-    Autobot autobot1;
-    Autobot autobotQ;
-    EXPECT_EQ(Autobot::autobotCounter, 3);
+    autobot.setName("Bumblebee");
+    autobot.setHeight(15);
+    EXPECT_EQ(autobot.getName(), "Bumblebee");  
+    EXPECT_EQ(autobot.getHeight(), 15);
 }
 
-// Minicons
-// We initialized the value of the counter 
+// --- Minicon Tests ---
 int Minicon::miniconCounter = 0; 
-TEST(MiniconTest, miniconsCount) {
-    Minicon minicon; 
-    EXPECT_EQ(Minicon::miniconCounter, 1); 
+
+TEST(MiniconTest, CounterIncrements) {
+    int initialCount = Minicon::miniconCounter;
+    Minicon mini1;
+    EXPECT_EQ(Minicon::miniconCounter, initialCount + 1);
 }
 
-// Decepticons
-TEST(DecepticonTest, decepticonGetName) { 
-    Decepticon decepticon; 
-    decepticon.setName("google");
-    EXPECT_EQ(decepticon.getName(), "google");
+TEST(MiniconTest, SmallSize) {
+    Minicon minicon;
+    minicon.setHeight(5);
+    EXPECT_LT(minicon.getHeight(), 10); 
+}
+
+// --- Decepticon Tests ---
+TEST(DecepticonTest, EvilNameAssignment) {
+    Decepticon decepticon;
+    decepticon.setName("Starscream");
+    EXPECT_EQ(decepticon.getName(), "Starscream");
+}
+
+TEST(DecepticonTest, TallHeight) {
+    Decepticon decepticon;
+    decepticon.setHeight(50);
+    EXPECT_GT(decepticon.getHeight(), 30); 
+}
+
+
+TEST(InteractionTest, AutobotVsDecepticonNames) {
+    Autobot autobot;
+    Decepticon decepticon;
+    autobot.setName("Ironhide");
+    decepticon.setName("Soundwave");
+    EXPECT_NE(autobot.getName(), decepticon.getName()); 
 }
 
 int main(int argc, char **argv) {
